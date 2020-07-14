6d91ae8d74e8ab549b5be1184e395cd4fe96b4af
Andreas Hege <a.hege@rac.de>
2020-07-14 14:56:56 +0200

    Added the demos from the wiki to code
    Signed-off-by: Andreas Hege <a.hege@rac.de>

--- java/src/test/java/net/asam/openscenario/v1_0/test/TestSimpleDemos.java
+++ java/src/test/java/net/asam/openscenario/v1_0/test/TestSimpleDemos.java
@@ -17,28 +17,25 @@
  
 package net.asam.openscenario.v1_0.test;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
-import com.sun.org.apache.xerces.internal.xni.parser.XMLParseException;
 
 import net.asam.openscenario.common.ErrorLevel;
-import net.asam.openscenario.common.FileContentMessage;
-import net.asam.openscenario.common.IParserMessageLogger;
 import net.asam.openscenario.common.SimpleMessageLogger;
-import net.asam.openscenario.common.Textmarker;
 import net.asam.openscenario.loader.FileResourceLocator;
 import net.asam.openscenario.loader.IScenarioLoader;
 import net.asam.openscenario.loader.IScenarioLoaderFactory;
-import net.asam.openscenario.loader.MessageLoggerDecorator;
 import net.asam.openscenario.loader.ScenarioLoaderException;
+import net.asam.openscenario.v1_0.api.IAxle;
+import net.asam.openscenario.v1_0.api.IAxles;
+import net.asam.openscenario.v1_0.api.ICatalogReference;
 import net.asam.openscenario.v1_0.api.IFileHeader;
 import net.asam.openscenario.v1_0.api.IOpenScenario;
-import net.asam.openscenario.v1_0.impl.OpenScenarioImpl;
+import net.asam.openscenario.v1_0.api.IScenarioObject;
+import net.asam.openscenario.v1_0.api.IVehicle;
+import net.asam.openscenario.v1_0.catalog.CatalogHelper;
+import net.asam.openscenario.v1_0.loader.XmlScenarioImportLoaderFactory;
 import net.asam.openscenario.v1_0.loader.XmlScenarioLoaderFactory;
 
 public class TestSimpleDemos extends TestBase{
@@ -71,6 +68,65 @@ public class TestSimpleDemos extends TestBase{
 
 		
 	}
+	
+	@Test
+    public void testImportDemo() throws ScenarioLoaderException{
+      
+      // Creating a message Logger to pick up the messages
+	  SimpleMessageLogger messageLogger = new SimpleMessageLogger(ErrorLevel.INFO);
+	  
+	  // create another messageLogger for logging the messages that occur from imported files
+	  SimpleMessageLogger catalogMessageLogger = new SimpleMessageLogger(ErrorLevel.INFO);
+      
+	  
+      // Instantiating the factory
+      IScenarioLoaderFactory loaderFactory = new XmlScenarioImportLoaderFactory(
+          catalogMessageLogger,getResourceFile("simpleImport/simpleImport.xosc").getAbsolutePath());
+
+      // Creating the loader with a file resource locator (we are reading directly from a file system)
+      IScenarioLoader loader = loaderFactory.createLoader(new FileResourceLocator());
+      
+      // Loading the scenario
+      IOpenScenario openScenario = (IOpenScenario)  loader.load(messageLogger).getAdapter(IOpenScenario.class);
+      
+      // Get the list of scenario objects
+      List<IScenarioObject> scenarioObjects = openScenario.getOpenScenarioCategory().getScenarioDefinition().getEntities().getScenarioObjects();
+      
+      
+      for (IScenarioObject scenarioObject: scenarioObjects)
+      {
+          // Access the object that is imported from a catalog with the name "Ego"
+          if (scenarioObject.getName().equals("Ego"))
+          {
+              // Get the catalog reference
+              ICatalogReference catalogReference = scenarioObject.getEntityObject().getCatalogReference();
+              
+              if (catalogReference != null && catalogReference.getEntryName().equals("car_white"))
+              {
+                  // Now check the type.
+                  if (CatalogHelper.isVehicle(catalogReference.getRef()))
+                  {
+                    IVehicle vehicle = CatalogHelper.asVehicle(catalogReference.getRef());
+                    // Now you can access the resolved vehicle
+                    IAxles axles = vehicle.getAxles();
+                    // get the additonal axles
+                    List<IAxle> additionalAxles = axles.getAdditionalAxles();
+                    if (additionalAxles == null || additionalAxles.size() == 0)
+                    {
+                      System.out.println("Ego has 2 axles (front, rear)");
+                    }else
+                    {
+                      System.out.println("Ego has "+ (2+additionalAxles.size()) +" axles (front, rear and "+ additionalAxles.size()+" addtional axles");
+                    }
+                    
+                    
+                  }
+              }
+          }
+      }
+
+        
+    }
 
 	
 	
