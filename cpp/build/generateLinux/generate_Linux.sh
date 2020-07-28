00ee2b6363c152f609144b694a972d080ae6cb25
Dr.-Ing. Dieter Finkenzeller <deakon@tri-nitro.com>
2020-07-28 15:01:58 +0200

    - modified build scripts for Linux

--- cpp/build/generateLinux/generate_Linux.sh
+++ cpp/build/generateLinux/generate_Linux.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-# TODO: check if cmake is installed
-# TODO: add script dir
-# TODO: add root-dir
+# Check if cmake is installed
+command -v cmake >/dev/null 2>&1 || { echo >&2 "cmake is required to build the software. Please install it. Aborting."; exit 1; }
+
+# Script-Dir: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Root-Dir
+ROOT_DIR=`pwd`
+while [ ! -d $ROOT_DIR/.root ]; do
+    ROOT_DIR=`realpath -s $ROOT_DIR/..`
+    if [ "/" == "$ROOT_DIR" ]; then
+        echo ".root not found! Terminating..."
+        exit -1
+    fi
+done
+echo $ROOT_DIR
 
 # Parse parameters
 . ${SCRIPT_DIR}/parse_generate_params.sh $1 $2
 
-# TODO: create build folder
+# Create build folder
+BUILDFOLDER="gcc_${PLATFORM_NAME}_${BUILD_TARGET}"
+mkdir -p "${BUILDFOLDER}"
+cd "${BUILDFOLDER}"
 
-# TODO: call cmake
-. ../../../Rac.Core.Cpp-src/CMake-Helper/generate_unix_common.sh $*
+# Call cmake
+cmake -DBUILD_TARGET_PARAM="${BUILD_TARGET}" -DPLATFORM_PARAM="${PLATFORM_NAME}" $ROOT_DIR/
 
-# TODO: build solution if requested
+# Build solution if requested
+if [ $MAKE != "yes" ]
+    then
+        exit 0
+fi
+make -j4
