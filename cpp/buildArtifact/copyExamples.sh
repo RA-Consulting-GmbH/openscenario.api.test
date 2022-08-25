004729b7ffa93dcd94a260cf63d71a586b544217
Dr.-Ing. Dieter Finkenzeller <deakon@tri-nitro.com>
2022-08-25 11:30:48 +0200

    modified two Linux bash scripts
    Signed-off-by: Dr.-Ing. Dieter Finkenzeller <deakon@tri-nitro.com>

--- cpp/buildArtifact/copyExamples.sh
+++ cpp/buildArtifact/copyExamples.sh
@@ -6,19 +6,19 @@ cd ${SCRIPT_DIR}
 
 
 # copy examples for release
-echo "rm -rf ${SCRIPT_DIR}/../build/output/Linux_shared/Release/examples"
-rm -rf ${SCRIPT_DIR}/../build/output/Linux_shared/Release/res
-echo "mkdir -p ${SCRIPT_DIR}/../build/output/Linux_shared/Release/examples"
-mkdir -p ${SCRIPT_DIR}/../build/output/Linux_shared/Release/res
-echo "cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/output/Linux_shared/Release"
-cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/output/Linux_shared/Release
+echo "rm -rf ${SCRIPT_DIR}/../build/cgReleaseMakeShared/applications/openScenarioReader/res"
+rm -rf ${SCRIPT_DIR}/../build/cgReleaseMakeShared/applications/openScenarioReader/res
+echo "mkdir -p ${SCRIPT_DIR}/../build/cgReleaseMakeShared/applications/openScenarioReader/res"
+mkdir -p ${SCRIPT_DIR}/../build/cgReleaseMakeShared/applications/openScenarioReader/res
+echo "cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/cgReleaseMakeShared/applications/openScenarioReader/res"
+cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/cgReleaseMakeShared/applications/openScenarioReader/res
 
 
 # copy examples for debug
-echo "rm -rf ${SCRIPT_DIR}/../build/output/Linux_shared/Debug/examples"
-rm -rf ${SCRIPT_DIR}/../build/output/Linux_shared/Debug/res
-echo "mkdir -p ${SCRIPT_DIR}/../build/output/Linux_shared/Debug/examples"
-mkdir -p ${SCRIPT_DIR}/../build/output/Linux_shared/Debug/res
-echo "cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/output/Linux_shared/Debug"
-cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/output/Linux_shared/Debug
+echo "rm -rf ${SCRIPT_DIR}/../build/cgDebugMakeShared/applications/openScenarioReader/res"
+rm -rf ${SCRIPT_DIR}/../build/cgDebugMakeShared/applications/openScenarioReader/res
+echo "mkdir -p ${SCRIPT_DIR}/../build/cgDebugMakeShared/applications/openScenarioReader/res"
+mkdir -p ${SCRIPT_DIR}/../build/cgDebugMakeShared/applications/openScenarioReader/res
+echo "cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/cgDebugMakeShared/applications/openScenarioReader/res"
+cp -r ${SCRIPT_DIR}/../../../doc/examples ${SCRIPT_DIR}/../build/cgDebugMakeShared/applications/openScenarioReader/res
 
